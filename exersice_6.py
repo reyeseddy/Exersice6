e2eb5391bc97a8401d430867225c4afcd8a3d83c
reyeseddy <ereyes7@umd.edu>
2021-10-20 23:32:45 -0400

    Create exersice_6.py

---
+++ exersice_6.py
@@ -0,0 +1,12 @@
+"""
+Driver: Eduardo Reyes
+Navigator: Jorge Argueta
+Assignment: Exercise 6
+INST326 
+DATE: 10_20_21
+Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~ 
+"""
+
+""" This program prints Hello"""
+
+print("Hello")
